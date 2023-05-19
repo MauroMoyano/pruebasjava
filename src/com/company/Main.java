9006e3b89731580d4080a2c082da95cad45803d9
MauroMoyano <mauromoyanodev@gmail.com>
2023-05-18 23:39:22 -0300

    Preparandome para entrar en el terreno desconocido de Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code heres
         System.out.println("mauro esta programando en java");
+        System.out.println("mauro necesito perfeccionarme en este lenguaje");
     }
 }
+
